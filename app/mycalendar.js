2238d63ba0368b64571794f6bd715abc5ffa7a8d
Anna <anna_u@live.com>
2011-08-16 22:16:49 +0200

    finished button (Add to calendar) everywhere. Outlook button doesn't work for now

--- app/mycalendar.js
+++ app/mycalendar.js
@@ -6,8 +6,7 @@
 	var loc=dat[1];
 	var docId=dat[2];
 	var desc="";
-//	console.log(dat);
-	
+console.log(dat);
 		
 	var startTime=[];
 	var endTime=[];
@@ -68,6 +67,6 @@ function parsingDate(dateData){
 
 
   $(function () {
-	$('#basicICal').icalendar($.extend({sites:['google','yahoo'], copyFlash:''}, calendarEvent));
+	$('#basicICal').icalendar($.extend({sites:['google','yahoo','outlook']}, calendarEvent));
 
 });
